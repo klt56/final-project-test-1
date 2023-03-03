5bac3d9992e82c0b42bb1abe995bbdf2f5e425ed
klt <letourneurkevin8@gmail.com>
2023-03-03 15:49:53 +0100

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  devise_for :users,
+             controllers: {
+               sessions: 'users/sessions',
+               registrations: 'users/registrations'
+             }
+  get '/member-data', to: 'members#show'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  scope '/checkout' do
+    post 'create', to: 'checkout#create', as: 'checkout_create'
+    get 'success', to: 'checkout#success', as: 'checkout_success'
+    get 'cancel', to: 'checkout#cancel', as: 'checkout_cancel'
+end
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
